461dc9561069a8e0f966b132ac04f90df8dffb91
arghaahit <argha.ahit@gmail.com>
2024-08-21 19:08:14 +0530

    changes

---
+++ lib/screens/auth/service/step.formdata.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class HoldStepFormData{
+  static String? username;
+  static String? name;
+  static String? lastname;
+  static String? profilepic;
+  static String? passowrd;
+  static String? bio;
+  static String? email;
+  static String? phone;
+  static String? accounttype;
+  static String? countrycode;
+}
